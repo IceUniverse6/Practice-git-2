927fa38930ce36c6567d629c46cb9cbc9c8e4994
IceUniverse6 <ali84azizi2005@gmail.com>
2025-04-19 05:22:30 +0330

    Second Commit

--- git-project/main.cpp
+++ git-project/main.cpp
@@ -4,7 +4,11 @@ using namespace std;
 void printHello(){
     cout << "Hello, World!" << endl;
 }
+void printID(){
+    cout << "4033613058" << endl;
+}
 int main(){
     printHello();
+    printID();
     return 0;
 }
