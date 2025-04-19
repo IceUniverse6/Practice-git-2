9f779473f197b4b12284bbc366e96d07ee70133a
IceUniverse6 <ali84azizi2005@gmail.com>
2025-04-19 05:25:51 +0330

    Revert "Second Commit"
    This reverts commit 927fa38930ce36c6567d629c46cb9cbc9c8e4994.

--- git-project/main.cpp
+++ git-project/main.cpp
@@ -4,11 +4,7 @@ using namespace std;
 void printHello(){
     cout << "Hello, World!" << endl;
 }
-void printID(){
-    cout << "4033613058" << endl;
-}
 int main(){
     printHello();
-    printID();
     return 0;
 }
